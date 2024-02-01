926375fbf7cc417901f129923aa8c7fd3a0dd5bf
Akshay Singh <akshay.singh@slrtce.in>
2024-02-01 15:43:01 +0530

    Day1

---
+++ day1.js
@@ -0,0 +1,13 @@
+const fs = require('fs').promises;
+
+async function readFileContent(filePath) {
+  try {
+    const content = await fs.readFile(filePath, 'utf-8');
+    console.log(content);
+  } catch (error) {
+    console.error(`Error reading file: ${error.message}`);
+  }
+}
+readFileContent('test-files/file1.txt');
+readFileContent('test-files/empty-file.txt');
+readFileContent('test-files/nonexistent-file.txt');

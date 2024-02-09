58e1f9282058d75f896972d81e42e1e2b7db8094
Akshay Singh <akshay.singh@slrtce.in>
2024-02-09 20:36:19 +0530

    Day9

---
+++ day9.js
@@ -0,0 +1,18 @@
+Which of the following are examples of node modules?
+
+a. Express
+
+b. Body-parser
+
+c. Socket.io
+
+d. All of the above
+
+
+d. All of the above
+
+Express, Body-parser, and Socket.io are all examples of Node.js modules commonly used in web development.
+
+Express: A web application framework that simplifies the development of web applications in Node.js.
+Body-parser: Middleware for handling HTTP request body parsing, often used with Express to parse incoming request bodies.
+Socket.io: A library for real-time web applications that enables bidirectional communication between web clients and servers.
